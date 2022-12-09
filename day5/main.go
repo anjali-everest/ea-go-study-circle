47c110ac521c7d1c4bc8662a5dcce7033f248e26
Gaddam Anjali <anjali.gaddam@everest.engineering>
2022-12-09 11:23:46 +0530

    Day-5: Go routine Example - Make go routine work by adding sleep time

--- day5/main.go
+++ day5/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "time"
 
 func greet(name string) {
 	fmt.Println("Hello ", name)
@@ -9,4 +10,5 @@ func greet(name string) {
 func main() {
 	go greet("Gopher")
 	fmt.Println("Done")
+	time.Sleep(time.Millisecond * 5)
 }
