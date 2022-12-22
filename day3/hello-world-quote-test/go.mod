759210f5379641fe4eebed37fdbe0c23cbec73c8
Gaddam Anjali <anjali.gaddam@everest.engineering>
2022-12-22 12:38:53 +0530

    Day-3, Go Modules - Implement a sample Hello world module that can be reusuable like other go repos

---
+++ day3/hello-world-quote-test/go.mod
@@ -0,0 +1,5 @@
+module hello-world-quote-test
+
+go 1.15
+
+require github.com/anjali-everest/hello-world-quote v0.1.0
---
+++ day3/hello-world-quote-test/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/anjali-everest/hello-world-quote"
+
+func main() {
+	hello_world_quote.SayHello()
+}
