160991e3a2e62b5e265ef9a04fcf640d49df65bd
Gaddam Anjali <anjali.gaddam@everest.engineering>
2022-12-25 15:38:52 +0530

    Day-8, Select - Impelemnt basic example of select statement

---
+++ day8/session/store_select.go
@@ -0,0 +1,30 @@
+package session
+
+type store interface {
+	sell() string
+}
+
+type buyer struct {
+	store1 store
+	store2 store
+}
+
+func deliver(store store) <-chan string {
+	channel := make(chan string)
+	go func() {
+		channel <- store.sell()
+	}()
+	return channel
+}
+
+func (b buyer) buy() string {
+	order1 := deliver(b.store1)
+	order2 := deliver(b.store2)
+
+	select {
+	case item := <-order1:
+		return item
+	case item := <-order2:
+		return item
+	}
+}
---
+++ day8/session/store_select_test.go
@@ -0,0 +1,28 @@
+package session
+
+import (
+	"testing"
+	"time"
+)
+
+type mockStore struct {
+	name string
+	time time.Duration
+}
+
+func (m mockStore) sell() string {
+	time.Sleep(m.time)
+	return m.name
+}
+
+func TestDeliver(t *testing.T) {
+	mockStore1 := mockStore{name: "store1", time: 10 * time.Millisecond}
+	mockStore2 := mockStore{name: "store2", time: 20 * time.Millisecond}
+
+	buyer := buyer{mockStore1, mockStore2}
+	order := buyer.buy()
+
+	if order != mockStore1.name {
+		t.Errorf("%s is not an expected store", order)
+	}
+}
