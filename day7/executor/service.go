613eeae0245b8c81abc48532f996303e3c0251e0
Gaddam Anjali <anjali.gaddam@everest.engineering>
2022-12-25 14:03:28 +0530

    Day-7, Job Executor Service, Wait Group - Implement Run functionality along with channel and wait group

--- day7/executor/service.go
+++ day7/executor/service.go
@@ -1,21 +1,35 @@
 package executor
 
-import "sync"
+import (
+	"sync"
+)
 
 type Service struct {
+	jobChan chan func()
 }
 
-func (s Service) Start(workersCnt int, setup func()) {
+func (s *Service) Start(workersCnt int, setup func()) {
+	jobChan := make(chan func())
+	s.jobChan = jobChan
 	wg := sync.WaitGroup{}
 	wg.Add(workersCnt)
+
 	for i := 0; i < workersCnt; i++ {
-		go worker(setup, &wg)
+		go worker(setup, &wg, jobChan)
 	}
 
 	wg.Wait()
 }
 
-func worker(setup func(), wg *sync.WaitGroup) {
+func (s Service) Run(job func()) {
+	s.jobChan <- job
+}
+
+func worker(setup func(), wg *sync.WaitGroup, jobChan chan func()) {
 	setup()
 	wg.Done()
+	for {
+		job := <-jobChan
+		job()
+	}
 }
